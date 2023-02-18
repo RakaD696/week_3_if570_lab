ac8ad09cedfb6133e0e7b3f6503e39766d083632
RakaD696 <Raka.danendra@student.umn.ac.id>
2023-02-18 14:34:49 +0700

    Exploring default values and compact functions

--- untitled/src/main/kotlin/Hello.kt
+++ untitled/src/main/kotlin/Hello.kt
@@ -1,72 +1,49 @@
-import java.util.* // required import
-fun randomDay() : String {
-    val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday",
-        "Friday", "Saturday", "Sunday")
-    return week[Random().nextInt(week.size)]
-}
-fun fishFood (day : String) : String {
-    return when (day) {
-        "Monday" -> "flakes"
-        "Wednesday" -> "redworms"
-        "Thursday" -> "granules"
-        "Friday" -> "mosquitoes"
-        "Sunday" -> "plankton"
-        else -> "nothing"
-    }
-}
-fun feedTheFish() {
-    val day = randomDay()
-    val food = fishFood(day)
-    println ("Today is $day and the fish eat $food")
-}
-fun main(args: Array<String>) {
-    feedTheFish()
 
 
-    fun swim(speed: String = "fast") {
-        println("The fish is swimming $speed")
-    }
+val decorations = listOf ("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
 
-    fun main() {
-        // Call swim() using the default parameter value
-        swim()
 
-        // Call swim() and pass the speed parameter without a name
-        swim("slow")
 
-        // Call swim() by naming the speed parameter
-        swim(speed = "turtle-like")
-    }
-
-    fun shouldChangeWater(day: String, temperature: Int = 22, dirty: Int = 20): Boolean {
-        return when {
-            temperature > 30 -> true
-            dirty > 30 -> true
-            day == "Sunday" -> true
-            else -> false
-        }
-    }
-
-    fun feedTheFish() {
-        val day = randomDay()
-        val food = fishFood(day)
-        println("Today is $day and the fish eat $food")
-        println("Change water: ${shouldChangeWater(day)}")
-    }
+fun main() {
+    val decorations = listOf(
+        "rock", "pagoda", "plastic plant", "alligator",
+        "flowerpot"
+    )
+    // eager, creates a new list
+    val eager = decorations.filter { it[0] == 'p' }
+    println("eager: $eager")
+// lazy, will wait until asked to evaluate
+    val filtered = decorations.asSequence().filter { it[0] == 'p' }
+    println("filtered: $filtered")
+    // force evaluation of the lazy list
+    val newList = filtered.toList()
+    println("new list: $newList")
 }
 
-fun isTooHot(temperature: Int) = temperature > 30
-fun isDirty(dirty: Int) = dirty > 30
-fun isSunday(day: String) = day == "Sunday"
-
-fun shouldChangeWater (day: String, temperature: Int = 22, dirty: Int = 20): Boolean {
-    return when {
-        isTooHot(temperature) -> true
-        isDirty(dirty) -> true
-        isSunday(day) -> true
-        else -> false
-    }
+val lazyMap = decorations.asSequence().map {
+    println("access: $it")
+    it
 }
+println("lazy: $lazyMap")
+println("-----")
+println("first: ${lazyMap.first()}")
+println("-----")
+println("all: ${lazyMap.toList()}")
+
+val lazyMap2 = decorations.asSequence().filter {it[0] == 'p'}.map {
+    println("access: $it")
+    it
+}
+println("-----")
+println("filtered: ${lazyMap2.toList()}")
+
+val mysports = listOf("basketball", "fishing", "running")
+val myplayers = listOf("LeBron James", "Ernest Hemingway", "Usain Bolt")
+val mycities = listOf("Los Angeles", "Chicago", "Jamaica")
+val mylist = listOf(mysports, myplayers, mycities) // list of lists
+println("-----")
+println("Flat: ${mylist.flatten()}")
+
 
 
 
