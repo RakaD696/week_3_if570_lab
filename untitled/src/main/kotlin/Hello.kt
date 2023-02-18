00f75b86d7eeebdedd5819b207ba59069157acfe
RakaD696 <Raka.danendra@student.umn.ac.id>
2023-02-18 14:20:46 +0700

    Learning more about functions

--- untitled/src/main/kotlin/Hello.kt
+++ untitled/src/main/kotlin/Hello.kt
@@ -1,47 +1,74 @@
 import java.util.* // required import
-
-fun main() {
-    val isUnit = println("this is an expression")
-    println(isUnit) // prints "kotlin.Unit"
-
-    val temperature = 10
-    val message = "The water temperature is ${ if (temperature > 50) "too warm" else "OK"
-    }."
-    println(message)
-
+fun randomDay() : String {
+    val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday",
+        "Friday", "Saturday", "Sunday")
+    return week[Random().nextInt(week.size)]
 }
-
-
-
+fun fishFood (day : String) : String {
+    return when (day) {
+        "Monday" -> "flakes"
+        "Wednesday" -> "redworms"
+        "Thursday" -> "granules"
+        "Friday" -> "mosquitoes"
+        "Sunday" -> "plankton"
+        else -> "nothing"
+    }
+}
 fun feedTheFish() {
     val day = randomDay()
-    val food = "pellets"
+    val food = fishFood(day)
     println ("Today is $day and the fish eat $food")
 }
 fun main(args: Array<String>) {
     feedTheFish()
-}
-    fun randomDay() : String {
-        val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday",
-            "Friday", "Saturday", "Sunday")
-        return week[Random().nextInt(week.size)]
+
+
+    fun swim(speed: String = "fast") {
+        println("The fish is swimming $speed")
     }
 
-fun fishFood (day : String) : String {
-    val food: String
-    when (day) {
-        "Monday" -> food = "flakes"
-        "Wednesday" -> food = "redworms"
-        "Thursday" -> food = "granules"
-        "Friday" -> food = "mosquitoes"
-        "Sunday" -> food = "plankton"
-        else -> food = "nothing"
+    fun main() {
+        // Call swim() using the default parameter value
+        swim()
+
+        // Call swim() and pass the speed parameter without a name
+        swim("slow")
+
+        // Call swim() by naming the speed parameter
+        swim(speed = "turtle-like")
     }
-    return food
+
+    fun shouldChangeWater(day: String, temperature: Int = 22, dirty: Int = 20): Boolean {
+        return when {
+            temperature > 30 -> true
+            dirty > 30 -> true
+            day == "Sunday" -> true
+            else -> false
+        }
+    }
+
     fun feedTheFish() {
         val day = randomDay()
         val food = fishFood(day)
         println("Today is $day and the fish eat $food")
+        println("Change water: ${shouldChangeWater(day)}")
     }
 }
+
+fun isTooHot(temperature: Int) = temperature > 30
+fun isDirty(dirty: Int) = dirty > 30
+fun isSunday(day: String) = day == "Sunday"
+
+fun shouldChangeWater (day: String, temperature: Int = 22, dirty: Int = 20): Boolean {
+    return when {
+        isTooHot(temperature) -> true
+        isDirty(dirty) -> true
+        isSunday(day) -> true
+        else -> false
+    }
+}
+
+
+
+
 
