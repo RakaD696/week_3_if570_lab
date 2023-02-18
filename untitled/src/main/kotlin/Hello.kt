5ecf3cc9735e50b6138518a9dd86596a7ce89ee3
RakaD696 <Raka.danendra@student.umn.ac.id>
2023-02-18 14:04:16 +0700

    Learning why (almost) everything has a value

--- untitled/src/main/kotlin/Hello.kt
+++ untitled/src/main/kotlin/Hello.kt
@@ -1,3 +1,5 @@
+import java.util.* // required import
+
 fun main() {
     val isUnit = println("this is an expression")
     println(isUnit) // prints "kotlin.Unit"
@@ -8,3 +10,38 @@ fun main() {
     println(message)
 
 }
+
+
+
+fun feedTheFish() {
+    val day = randomDay()
+    val food = "pellets"
+    println ("Today is $day and the fish eat $food")
+}
+fun main(args: Array<String>) {
+    feedTheFish()
+}
+    fun randomDay() : String {
+        val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday",
+            "Friday", "Saturday", "Sunday")
+        return week[Random().nextInt(week.size)]
+    }
+
+fun fishFood (day : String) : String {
+    val food: String
+    when (day) {
+        "Monday" -> food = "flakes"
+        "Wednesday" -> food = "redworms"
+        "Thursday" -> food = "granules"
+        "Friday" -> food = "mosquitoes"
+        "Sunday" -> food = "plankton"
+        else -> food = "nothing"
+    }
+    return food
+    fun feedTheFish() {
+        val day = randomDay()
+        val food = fishFood(day)
+        println("Today is $day and the fish eat $food")
+    }
+}
+
