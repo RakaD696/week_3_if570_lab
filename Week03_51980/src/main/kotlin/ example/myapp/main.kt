3789ccd40c02c28fe01c3c1d7aa490ff5bb6bb66
RakaD696 <Raka.danendra@student.umn.ac.id>
2023-02-23 11:02:48 +0700

    abc

---
+++ Week03_51980/src/main/kotlin/ example/myapp/main.kt
@@ -0,0 +1,17 @@
+package  example.myapp
+
+fun buildAquarium() {
+    val myAquarium = Aquarium()
+}
+fun main() {
+    buildAquarium()
+}
+fun printSize(width: String, length: String, height: String) {
+    println("Width: $width cm " +
+            "Length: $length cm " +
+            "Height: $height cm ")
+}
+fun buildAquarium() {
+    val myAquarium = Aquarium()
+    myAquarium.printSize()
+}
